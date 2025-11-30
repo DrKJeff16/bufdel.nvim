d573326328a4c65f7f251c06ceff48b269b1a76d
Eric Wong <wsdjeg@outlook.com>
2025-11-30 23:20:23 +0800

    fix: add delete_buf function

--- lua/bufdel/init.lua
+++ lua/bufdel/init.lua
@@ -1,6 +1,17 @@
 local M = {}
 
+local function delete_buf(buf, opt) end
+
 function M.delete(buf, opt)
+	if type(buf) == "number" then
+		delete_buf(buf, opt)
+	elseif type(buf) == "function" then
+		for _, v in ipairs(vim.api.nvim_list_bufs()) do
+			if buf(v) then
+				delete_buf(v, opt)
+			end
+		end
+	end
 end
 
 return M
